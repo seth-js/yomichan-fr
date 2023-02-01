b4d6470b79310fa37fd139a76a5114fdaa5861ec
seth-js <83692925+seth-js@users.noreply.github.com>
2023-02-01 14:09:07 -0600

    Initial commit

---
+++ yomichan/sw.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020-2022  Yomichan Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+self.importScripts(
+    '/lib/parse5.js',
+    '/lib/wanakana.min.js',
+    '/js/core.js',
+    '/js/yomichan.js',
+    '/js/accessibility/accessibility-controller.js',
+    '/js/background/backend.js',
+    '/js/background/profile-conditions-util.js',
+    '/js/background/request-builder.js',
+    '/js/background/script-manager.js',
+    '/js/comm/anki-connect.js',
+    '/js/comm/clipboard-monitor.js',
+    '/js/comm/clipboard-reader.js',
+    '/js/comm/mecab.js',
+    '/js/data/anki-util.js',
+    '/js/data/database.js',
+    '/js/data/json-schema.js',
+    '/js/data/options-util.js',
+    '/js/data/permissions-util.js',
+    '/js/data/sandbox/array-buffer-util.js',
+    '/js/dom/simple-dom-parser.js',
+    '/js/extension/environment.js',
+    '/js/general/cache-map.js',
+    '/js/general/object-property-accessor.js',
+    '/js/general/regex-util.js',
+    '/js/general/text-source-map.js',
+    '/js/language/deinflector.js',
+    '/js/language/dictionary-database.js',
+    '/js/language/sandbox/dictionary-data-util.js',
+    '/js/language/sandbox/japanese-util.js',
+    '/js/language/translator.js',
+    '/js/media/audio-downloader.js',
+    '/js/media/media-util.js',
+    '/js/templates/template-patcher.js',
+    '/js/background/background-main.js'
+);
